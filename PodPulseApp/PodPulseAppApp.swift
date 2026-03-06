555ed5ff7634d68a5e66b1c7628e57cfdc007b8a
Mohamed Fawzy <m.fawzy.a@icloud.com>
2026-03-06 22:50:50 +0200

    Initial Commit

---
+++ PodPulseApp/PodPulseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodPulseAppApp.swift
+//  PodPulseApp
+//
+//  Created by Mohamed Fawzy on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PodPulseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
